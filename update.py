be269109551fa7f52bfa4cfecc4bd8d21004eff6
Stephen Lead <stephen.lead@gmail.com>
2018-01-18 16:16:25 +0100

    Replace iag. with bootleaf.

--- update.py
+++ update.py
@@ -9,8 +9,6 @@ import sys, os
 from distutils.dir_util import copy_tree
 
 destination = "/Library/WebServer/Documents/demos/bootleaf"
-
-# Find the path to the Bootleaf source code, which is a few directories up from this script
 source = "/Library/WebServer/Documents/iagcl/geo_web/Leaflet/bootleaf"
 
 # Copy/Update the entire Bootleaf /src/ folder, and the Bootleaf Checker in the /check/ folder
@@ -19,3 +17,21 @@ for dir in ['src','check']:
     outDir = os.path.join(destination, dir)
     print ("copying from " + inDir + " to " + outDir)
     copy_tree(inDir, outDir)
+
+    # Replace iag. with bootleaf.
+
+    for root, dirs, files in os.walk(outDir):
+        for file in files:
+            if file.endswith(".js"):
+                print(os.path.join(root, file))
+
+                # Read in the source file
+                with open(os.path.join(root, file), 'r') as f:
+                    contents = f.read()
+
+                    # Replace the target string
+                    contents = contents.replace('iag', 'bootleaf')
+
+                    # Write the file out again
+                    with open(os.path.join(root, file), 'w') as g:
+                      g.write(contents)
