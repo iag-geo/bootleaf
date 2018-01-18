0a934c2a7a22f539e98fff1e090a3a61d7ac551c
Stephen Lead <stephen.lead@gmail.com>
2018-01-18 16:20:36 +0100

    Also apply edits to the github.io version

--- update.py
+++ update.py
@@ -9,28 +9,29 @@ import sys, os
 from distutils.dir_util import copy_tree
 
 destination = "/Library/WebServer/Documents/demos/bootleaf"
-source = "/Library/WebServer/Documents/iagcl/geo_web/Leaflet/bootleaf"
+sources = ["/Library/WebServer/Documents/iagcl/geo_web/Leaflet/bootleaf", "/Library/WebServer/Documents/demos/slead.github.io/bootleaf"]
 
-# Copy/Update the entire Bootleaf /src/ folder, and the Bootleaf Checker in the /check/ folder
-for dir in ['src','check']:
-    inDir = os.path.join(source, dir)
-    outDir = os.path.join(destination, dir)
-    print ("copying from " + inDir + " to " + outDir)
-    copy_tree(inDir, outDir)
+for source in sources:
+    # Copy/Update the entire Bootleaf /src/ folder, and the Bootleaf Checker in the /check/ folder
+    for dir in ['src','check']:
+        inDir = os.path.join(source, dir)
+        outDir = os.path.join(destination, dir)
+        print ("copying from " + inDir + " to " + outDir)
+        copy_tree(inDir, outDir)
 
-    # Replace iag. with bootleaf.
+        # Replace iag. with bootleaf.
 
-    for root, dirs, files in os.walk(outDir):
-        for file in files:
-            if file.endswith(".js"):
-                print(os.path.join(root, file))
+        for root, dirs, files in os.walk(outDir):
+            for file in files:
+                if file.endswith(".js"):
+                    print(os.path.join(root, file))
 
-                # Read in the source file
-                with open(os.path.join(root, file), 'r') as f:
-                    contents = f.read()
+                    # Read in the source file
+                    with open(os.path.join(root, file), 'r') as f:
+                        contents = f.read()
 
-                    # Replace iag with bootleaf
-                    if (contents.find('iag') > -1):
-                        contents = contents.replace('iag', 'bootleaf')
-                        with open(os.path.join(root, file), 'w') as g:
-                          g.write(contents)
+                        # Replace iag with bootleaf
+                        if (contents.find('iag') > -1):
+                            contents = contents.replace('iag', 'bootleaf')
+                            with open(os.path.join(root, file), 'w') as g:
+                              g.write(contents)
